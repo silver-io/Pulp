511449371373868e143d835f3301443720d27ee9
silver-io <javier@silver.io>
2014-11-14 16:50:26 +0000

    creates model, migration and renders the restaurant in the views

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,16 @@ describe 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Crate')
+    end
+
+    it 'should display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('Crate')
+      expect(page).not_to have_content('No restaurants')
+    end
+  end
 end
